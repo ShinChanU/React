bab6e615153407f3e0a17e61e54e6cdff1f7c151
ShinChanU <tlscksdn963@naver.com>
2021-08-26 20:29:54 +0900

    react webpack studying

---
+++ zerocho/끝말잇기/WordRelay.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+  state = {
+
+  };
+
+  render() {
+
+  }
+}
+
+module.exports = WordRelay;
+
+// 파일을 분리할때 시작 두줄과 끝 두줄 꼭 적어줘야함
---
+++ zerocho/끝말잇기/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  name: 'wordRelay-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval', // 빠르게
+  resolve: {
+    extensions: ['.js', '.jsx'] // js, jsx를 알아서 찾아서 적용
+  },
+
+  entry: {
+    app: ['./client'], // client.jsx 속에 WordRelay.jsx 있어서 생략
+  }, // 입력
+  output: {
+    path: path.join(__dirname, 'dist'), // C:\users\.... 
+    filename: 'app.js'
+  }, // 출력
+};
