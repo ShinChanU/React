f13f36d22d2dd0b49e948c91871649d517aa74c4
ShinChanU <tlscksdn963@naver.com>
2021-08-29 20:03:17 +0900

    react hotLoading, change hooks in WordRelay

--- zerocho/끝말잇기/WordRelay.jsx
+++ zerocho/끝말잇기/WordRelay.jsx
@@ -1,56 +1,96 @@
 const React = require('react');
-const { Component } = React;
+// const { Component } = React; 
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-  state = {
-    word: '신찬우',
-    value: '',
-    result: '',
-  };
+// hooks
+const WordRelay = () => {
+  const [word, setWord] = useState('신찬우');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        word: this.state.value,
-        value: '',
-        result: '딩동댕',
-      });
-      this.input.focus();
+    if (word[word.length - 1] === value[0]) {
+      setWord(value);
+      setValue('');
+      setResult('딩동댕');
+      inputRef.current.focus();
     } else {
-      this.setState({
-        value: '',
-        result: '땡',
-      });
-      this.input.focus();
+      setValue('');
+      setResult('땡');
+      inputRef.current.focus();
     }
   };
 
-  onChangeInput = (e) => {
-    this.setState({
-      value: e.target.value,
-    })
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  input;
-
-  onRefInput = (c) => {
-    this.input = c;
-  };
-
-  render() {
-    return (
-      <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmitForm}>
-          <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-          <button>입력!!</button>
-        </form>
-        <div>{this.state.result}</div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputRef} value={value} onChange={onChangeInput} />
+        <button>클릭!!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
 }
+
+
+// class
+// class WordRelay extends Component {
+//   state = {
+//     word: '신찬우',
+//     value: '',
+//     result: '',
+//   };
+
+//   onSubmitForm = (e) => {
+//     e.preventDefault();
+//     if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+//       this.setState({
+//         word: this.state.value,
+//         value: '',
+//         result: '딩동댕',
+//       });
+//       this.input.focus();
+//     } else {
+//       this.setState({
+//         value: '',
+//         result: '땡',
+//       });
+//       this.input.focus();
+//     }
+//   };
+
+//   onChangeInput = (e) => {
+//     this.setState({
+//       value: e.target.value,
+//     })
+//   };
+
+//   input;
+
+//   onRefInput = (c) => {
+//     this.input = c;
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <div>{this.state.word}</div>
+//         <form onSubmit={this.onSubmitForm}>
+//           <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+//           <button>입력!!</button>
+//         </form>
+//         <div>{this.state.result}</div>
+//       </>
+//     );
+//   }
+// }
 
 module.exports = WordRelay;
 
--- zerocho/끝말잇기/webpack.config.js
+++ zerocho/끝말잇기/webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const webpack = require('webpack');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   name: 'wordRelay-setting',
-  mode: 'development', // 실서비스: production
+  // mode: 'development', // 실서비스: production
+  mode: isDevelopment ? 'development' : 'production',
   devtool: 'eval', // 빠르게
   resolve: {
     extensions: ['.js', '.jsx'] // js, jsx를 알아서 찾아서 적용
@@ -17,13 +22,48 @@ module.exports = {
       test: /\.jsx?/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [  // preset은 plugin의 모음, env.target.browsers 는 지원하는 브라우저 지정가능
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],  // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react'
+        ],
+        // plugins: [
+        //   '@babel/plugin-proposal-class-properties',
+        //   'react-refresh/babel',
+        // ],
+        plugins: [
+          isDevelopment && require.resolve('react-refresh/babel'),
+        ].filter(Boolean),
       },
     }],
   },
 
+  // plugins: [
+  //   new RefreshWebpackPlugin(
+  //     { overlay: false }
+  //   ),
+  // ],
+
+  plugins: [
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(
+      { overlay: false }
+    ),
+  ].filter(Boolean),
+
   output: {
-    path: path.join(__dirname, 'dist'), // C:\users\.... 
-    filename: 'app.js'
-  }, // 출력
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/',
+  },
+
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true
+  }
 };
