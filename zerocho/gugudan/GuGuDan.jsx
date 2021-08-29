24ca4e61b2f7cae1a2687e3ebc40de925f3f2eb9
ShinChanU <tlscksdn963@naver.com>
2021-08-29 16:10:57 +0900

    webpack target browsers

--- zerocho/gugudan/GuGuDan.jsx
+++ zerocho/gugudan/GuGuDan.jsx
@@ -1,12 +1,12 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React; // 반복 사용 x
 
 const GuGuDan = () => {
-  const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
-  const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
-  const [value, setValue] = React.useState('');
-  const [result, setResult] = React.useState('');
-  const inputRef = React.useRef(null);
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
 
   const onChangeInput = (e) => {
     setValue(e.target.value);
@@ -21,21 +21,21 @@ const GuGuDan = () => {
       setResult(`정답 ${value}`);
       inputRef.current.focus();
     } else {
-      setResult('땡!!!!!');
+      setResult('땡');
       setValue('');
       inputRef.current.focus();
     }
   };
 
   return (
-    <React.Fragment>
+    <>
       <div>{first} 곱하기 {second}는?</div>
       <form onSubmit={onSubmitForm}>
         <input ref={inputRef} type="number" value={value} onChange={onChangeInput} />
         <button>입력!</button>
       </form>
       <div>{result}</div>
-    </React.Fragment>
+    </> // jsx, fragment삭제 가능
   );
 }
 
--- zerocho/gugudan/webpack.config.js
+++ zerocho/gugudan/webpack.config.js
@@ -3,25 +3,32 @@ const path = require('path');
 module.exports = {
   name: 'GuGuDan-setting',
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'eval', //아니면 hidden-source-map
   resolve: {
     extensions: ['.js', '.jsx'],
   },
 
   entry: {
-    app: ['./client'],
+    app: ['./client'], // 확장자 생략, 위에 resolve에서 선언
   },
 
   module: {
     rules: [{
       test: /\.jsx?/,
-      loader: [
-        { loader: 'babel', query: { compact: false } },
-      ],
+      loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [  // preset은 plugin의 모음, env.target.browsers 는 지원하는 브라우저 지정가능
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],  // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react'
+        ],
+        plugins: [],
       },
-    }],
+    }]
   },
 
   output: {
@@ -29,3 +36,5 @@ module.exports = {
     filename: 'app.js',
   },
 };
+
+// 순서는 (mode) -> entry -> module -> plugin -> output  = 핵심 5가지
