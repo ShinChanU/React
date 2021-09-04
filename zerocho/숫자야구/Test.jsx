1c58182851237a01a88933d8a84b08cb8661e190
ShinChanU <tlscksdn963@naver.com>
2021-09-04 15:28:45 +0900

    react optimize from shouldComponentUpdate

---
+++ zerocho/숫자야구/Test.jsx
@@ -0,0 +1,36 @@
+// 렌더링은 props, state가 변화할 때 발생
+// 그러나 setState()시에 아무 값을 주지 않아도 렌더링 되는 현상이 발생할 수 있음
+// shouldComponentUpdate() 메소드를 사용해서 불필요한 렌더링을 방지하고 최적화를 할 수 있음
+// devtools > 설정 > highlight update 체크 > 렌더링시 컴포넌트 색깔 나옴
+
+import React, { Component } from 'react';
+
+class Test extends Component {
+  state = {
+    counter: 0,
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) { // 이 메소드를 삽입해서 불필요한 렌더링 방지, 최적화
+    if (this.state.counter !== nextState.counter) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onClickBtn = () => { // state 값이 변하지 않음
+    this.setState({
+
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickBtn}>클릭!</button>
+      </div>
+    );
+  }
+}
+
+export default Test;
--- zerocho/숫자야구/client.jsx
+++ zerocho/숫자야구/client.jsx
@@ -1,6 +1,7 @@
 // class, import, export default
 // import React from 'react';
 // import ReactDom from 'react-dom';
+// import Test from './Test';
 // import NumberBaseballClass from './NumberBaseballClass';
 
 // ReactDom.render(<NumberBaseballClass />, document.querySelector("#root"));
