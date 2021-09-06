ed66376c3a37745b637e19fb7026598099da29e0
ShinChanU <tlscksdn963@naver.com>
2021-09-06 20:50:18 +0900

    lotto build doing

---
+++ zerocho/5.로또/LottoClass.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+function getWinNumbers() {
+  console.log('getWinNumbers');
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+class LottoClass extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <>
+        <div>hello</div>
+      </>
+    );
+  }
+}
+
+export default LottoClass;
---
+++ zerocho/5.로또/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import LottoClass from './LottoClass';
+
+ReactDom.render(<LottoClass />, document.querySelector("#root")); // <lotto /> x, 첫글자는 대문자 !
