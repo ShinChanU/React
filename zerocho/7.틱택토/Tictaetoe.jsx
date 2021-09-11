854b63169449db6b1032fe7fb4169117289bcd11
ShinChanU <tlscksdn963@naver.com>
2021-09-11 19:21:26 +0900

    rename Tictactoe build

---
+++ zerocho/7.틱택토/Tictaetoe.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tictactoe extends Component {
+  render() {
+    return (
+      <div>
+        Hello !!
+      </div>
+    );
+  }
+}
+
+export default Tictactoe;
---
+++ zerocho/7.틱택토/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Tictactoe from './Tictaetoe';
+
+ReactDom.render(<Tictactoe />, document.querySelector("#root"));
