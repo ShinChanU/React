82a9b401d65bc2421c4ddd631fabe2f1da8da208
ShinChanU <tlscksdn963@naver.com>
2021-10-08 00:01:28 +0900

    chap 8 done

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,12 @@
-import Counter from './Counter';
+// import Counter from './Counter';
 // import React from 'react';
-import Average from './Average';
+// import Average from './Average';
+// import LocalUseRef from './LocalUseRef';
+import Info3 from './Info3';
 
 const App = () => {
   return (
-    <Average />
+    <Info3 />
   );
 }
 
--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = (numbers) => {
   console.log('평균값 계산 중..');
@@ -10,22 +10,27 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
+    console.log("onChange");
+  }, []); // 컴포넌트가 처음 렌더링 될 때만 함수 생성
 
-  const onInsert = (e) => {
+  const onInsert = useCallback(() => {
+    console.log("onInsert");
     const nextList = list.concat(parseInt(number))
     setList(nextList);
     setNumber('');
-  };
+    inputEl.current.focus();
+  }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
+  // useMemo는 숫자, 문자열, 객체, useCallback은 함수
 
   const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input ref={inputEl} value={number} onChange={onChange} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((v, i) => (
